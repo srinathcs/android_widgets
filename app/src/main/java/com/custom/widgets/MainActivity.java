7187eab1e7381fc104994638fb4901bd90fdf274
Srinath <srinath001me@gmail.com>
2022-08-18 19:10:57 +0530

    testing

--- app/src/main/java/com/custom/widgets/MainActivity.java
+++ app/src/main/java/com/custom/widgets/MainActivity.java
@@ -2,14 +2,38 @@ package com.custom.widgets;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.drawable.shapes.Shape;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.add_text_view);
+        setContentView(R.layout.text_view_learing);
+        Button bt=findViewById(R.id.btnSimple);
+
+        TextView myTextView = findViewById(R.id.tvSetText);
+        myTextView.setText(R.string.tvSetText);
+        myTextView = findViewById(R.id.tvSetText);
+        myTextView.setTextColor(myTextView.getResources().getColor(R.color.teal_700));
+        myTextView.setTextSize(50);
+        myTextView.setBackgroundColor(myTextView.getResources().getColor(R.color.black));
+        myTextView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(getApplicationContext(),"Hai",Toast.LENGTH_SHORT).show();
+            }
+        });
+
+
+
+
+
 
     }
 }
